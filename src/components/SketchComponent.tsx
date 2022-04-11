1e12a82d9a1b5e759b817dda331e4f0c96dfca85
SushiWaUmai <eugene.bb1@gmail.com>
2022-04-11 12:24:44 +0200

    style: :art: move head tags to index.tsx

--- src/components/SketchComponent.tsx
+++ src/components/SketchComponent.tsx
@@ -1,21 +1,13 @@
 import React from "react";
 import { setup, draw } from "../utils/sketch";
 import dynamic from "next/dynamic";
-import Head from "next/head";
 
 const Sketch = dynamic(import("react-p5"), { ssr: false });
 
 interface SketchComponentProps {}
 
 const SketchComponent: React.FC<SketchComponentProps> = () => {
-  return (
-    <>
-      <Head>
-        <title>p5 + Next.js template</title>
-      </Head>
-      <Sketch setup={setup} draw={draw} />
-    </>
-  );
+  return <Sketch setup={setup} draw={draw} />;
 };
 
 export default SketchComponent;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
 import type { NextPage } from "next";
 import SketchComponent from "../components/SketchComponent";
-
+import Head from "next/head";
 
 const Home: NextPage = () => {
   return (
     <>
+      <Head>
+        <title>p5 + Next.js template</title>
+      </Head>
       <SketchComponent />
     </>
   );
